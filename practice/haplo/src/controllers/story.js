e93abd9de539c41d933cf35930d9bd0411347738
Matt Miller <matt123miller@hotmail.co.uk>
2019-03-10 22:31:41 +0000

    some windows package-lock and my windows vscode formats better.

--- practice/haplo/src/controllers/story.js
+++ practice/haplo/src/controllers/story.js
@@ -1,11 +1,11 @@
 //@ts-check
 
 let storyCache = {
-    '0' : { text: 'This is where your story begins'},
-    '01' : {},
-    '02' : {},
-    '03' : {},
-    '04' : {},
+    '0': { text: 'This is where your story begins' },
+    '01': {},
+    '02': {},
+    '03': {},
+    '04': {},
 };
 
 const directions = {
@@ -20,19 +20,19 @@ const directions = {
 function index(req, res) {
 
     const currentStoryFragments = buildTemplatingData(0);
-    console.log({currentStoryFragments});
+    console.log({ currentStoryFragments });
 
     res.render("index.njk", currentStoryFragments);
 }
 
 function getFragment(req, res) {
 
-    const { fragmentID } = req.params;
+    const fragmentID = req.params.fragmentID || 0;
 
     const currentStoryFragments = buildTemplatingData(fragmentID);
-    
+
     console.log('getfragment call')
-    console.log({currentStoryFragments});
+    console.log({ currentStoryFragments });
 
     res.render("index.njk", currentStoryFragments);
 }
@@ -44,18 +44,18 @@ function postFragment(req, res) {
     const currentStoryId = req.body.currentStoryId;
     const directionKey = directions[bodyDirection];
 
-    const directionID = computeFragmentHash(currentStoryId,  directionKey);
+    const directionID = computeFragmentHash(currentStoryId, directionKey);
 
     storyCache[directionID] = { text: req.body[bodyDirection] };
-    
+
     const currentStoryFragments = buildTemplatingData(currentStoryId);
     // Also add the currentStoryId for good measure. Coudl do it in the above function but it feels better here.
     // currentStoryFragments.currentStoryId = currentStoryId;
-    
-    console.log({body: req.body});
-    console.log({storyCache});
-    console.log({currentStoryFragments});
-    console.log({bodyDirection});
+
+    console.log({ body: req.body });
+    console.log({ storyCache });
+    console.log({ currentStoryFragments });
+    console.log({ bodyDirection });
 
     res.render('index.njk', currentStoryFragments);
 }
